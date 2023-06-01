a09388c4feffe5a10da841d635c3e918f576a2f4
Anna <77854070+annavolovyk@users.noreply.github.com>
2023-06-01 22:21:23 +0300

    Update PersonTest.java

--- src/test/java/personTests/PersonTest.java
+++ src/test/java/personTests/PersonTest.java
@@ -6,43 +6,23 @@ import org.testng.annotations.Test;
 public class PersonTest {
     @Test
     public void testGetFirstName(){
-        Person person = new Person() {
-            @Override
-            public boolean isRetired() {
-                return false;
-            }
-        };
+        Person person = new Person();
         Assert.assertEquals(person.getFirstName(),"Anna", "This is a wrong name");
     }
 
     @Test
     public void testGetLastName(){
-        Person person = new Person() {
-            @Override
-            public boolean isRetired() {
-                return false;
-            }
-        };
+        Person person = new Person();
         Assert.assertEquals(person.getLastName(),"Smith", "This is a correct lastName");
     }
     @Test
     public void testGetAge(){
-        Person person = new Person() {
-            @Override
-            public boolean isRetired() {
-                return false;
-            }
-        };
+        Person person = new Person(); 
         Assert.assertEquals(person.getAge(),65, "This is man's age");
     }
     @Test
     public void testGetPartner(){
-        Person person = new Person() {
-            @Override
-            public boolean isRetired() {
-                return false;
-            }
-        };
+        Person person = new Person(); 
         Assert.assertEquals(person.getPartner(),"Mary", "This is a partner");
     }
 }
